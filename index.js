833f4624fca5822b917c3823132498b6568b1322
Arjun99ab <53882059+Arjun99ab@users.noreply.github.com>
2022-04-25 17:28:37 -0400

    fixed image scaling maybe to fit all screens

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+
+window.onload = (event) => {
+  console.log('page is fully loaded');
+  console.log(window.innerHeight);
+
+  var pfp = document.getElementById('pfp');
+  pfp.height = window.innerHeight;
+  pfp.width = window.innerHeight;
+
+
+};
+
 function githubClick() {
   window.open("https://github.com/Arjun99ab", "_blank");
 }
