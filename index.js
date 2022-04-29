fcc0d0e606d61347f0e692c5a245c46af3ee2467
Arjun99ab <53882059+Arjun99ab@users.noreply.github.com>
2022-04-29 09:48:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-
-
 window.onload = (event) => {
-  console.log('page is fully loaded');
+  console.log("page is fully loaded");
   console.log(window.innerHeight);
 
-  var pfp = document.getElementById('pfp');
+  var pfp = document.getElementById("pfp");
   pfp.height = window.innerHeight;
   pfp.width = window.innerHeight;
-
-
 };
 
 function githubClick() {
   window.open("https://github.com/Arjun99ab", "_blank");
+}
+function homeClick() {
+  $("body").load("index.html");
 }
