7212df9b83ebd589347abe60406f7c69b3fd26e4
Arjun99ab <53882059+Arjun99ab@users.noreply.github.com>
2022-04-29 18:35:20 -0400

    cool home button animation

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-
-
 window.onload = (event) => {
-  console.log('page is fully loaded');
+  console.log("page is fully loaded");
   console.log(window.innerHeight);
 
-  var pfp = document.getElementById('pfp');
+  var pfp = document.getElementById("pfp");
   pfp.height = window.innerHeight;
   pfp.width = window.innerHeight;
-
-
+  var homePfp = document.getElementById("homePfp");
+  homePfp.height = window.innerHeight;
+  homePfp.width = window.innerHeight;
 };
 
 function githubClick() {
   window.open("https://github.com/Arjun99ab", "_blank");
+}
+function homeClick() {
+  // $("body").load("index.html");
+  // window.history.pushState({}, "", "index.html");
+  window.location.replace("index.html");
 }
